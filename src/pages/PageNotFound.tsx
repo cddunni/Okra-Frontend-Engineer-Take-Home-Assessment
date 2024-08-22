dd09a9289461c25e3483bf8aa4151d9e8d9ee1a4
cddunni <oluwatimileyinakinpelu49@gmail.com>
2024-08-22 13:45:47 +0100

    page not found page rework

---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,23 @@
+import NotFound from "@/assets/svg/404.svg?react"
+import { useNavigate } from "react-router-dom"
+import Button from "@/components/button/Button"
+import React from "react"
+import '../assets/scss/pages/_page-not-found.scss'
+
+export default function PageNotFound() {
+    const navigate = useNavigate()
+
+    return (
+        <div className="page-not-found-container">
+            <NotFound className="mb-3" />
+            <p className="font-pptelegraph_ub title">Something went wrong</p>
+            <p className="font-pptelegraph_r description">Sorry, the page you’re looking for does not exist</p>
+            <Button
+                btnText="Back"
+                theme="primary"
+                variant='back-button'
+                onClick={()=> navigate('/')}
+            />
+        </div>
+    )
+}
