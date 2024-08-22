08c0a37a62afb5a80178a07f2b66a68a825eb1e6
cddunni <oluwatimileyinakinpelu49@gmail.com>
2024-08-22 13:46:07 +0100

    success popup component rework

---
+++ src/components/success/Success.tsx
@@ -0,0 +1,17 @@
+import SuccessIllustration from '@/assets/svg/success.svg?react';
+import React from 'react';
+import './_success.scss'
+
+const Success = () => {
+  return (
+    <section className='success-container'>
+        <div className='text-container'>
+            <p className='title'>Thank you</p>
+            <p className='mt-2 text-secondary md:text-xl text-base md:leading-8 leading-6 description'>A member of our team will be in touch with you shortly.</p>
+        </div>
+        <SuccessIllustration/>
+    </section>
+  )
+}
+
+export default Success
